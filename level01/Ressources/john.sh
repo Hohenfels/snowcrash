89f11307778f7f7baadaa3d786db891ea0cdddbb
Hohenfels <hapeiste@gmail.com>
2020-02-25 17:27:29 +0100

    0 1 2 3

--- level01/Ressources/john.sh
+++ level01/Ressources/john.sh
@@ -1,5 +1,5 @@
 curl -L https://www.openwall.com/john/k/john-1.9.0-jumbo-1.tar.gz > john.tar.gz && tar -xf john.tar.gz
 cd john-1.9.0-jumbo-1/src && ./configure && make -s clean && make -sj4
-sudo scp -P 4242 flag00@192.168.1.26:/etc/passwd passwd
+sudo scp -P 4242 level00@192.168.1.26:/etc/passwd passwd
 ./john-1.9.0-jumbo-1/run/john passwd
 ./john-1.9.0-jumbo-1/run/john passwd --show 
